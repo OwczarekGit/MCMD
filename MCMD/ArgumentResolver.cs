7d60cffadd48def0dc627ecc0fe1c22d0a27514a
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-05-29 20:40:14 +0200

    Added flags for specifying game version target and mod loader target.

--- MCMD/ArgumentResolver.cs
+++ MCMD/ArgumentResolver.cs
@@ -1,7 +1,52 @@
+using System;
+using System.Collections.Generic;
+
 namespace MCModDownloader
 {
     public class ArgumentResolver
     {
-        public ArgumentResolver(){}
+        public List<String> arguments = new List<string>();
+        public ArgumentResolver(String[] args)
+        {
+            foreach (var arg in args)
+                arguments.Add(arg);
+            
+            processArguments();
+        }
+
+        private void processArguments()
+        {
+            try
+            {
+                for (int i=0; i < arguments.Count; i++)
+                {
+                    String currentArg = arguments[i];
+
+                    if (currentArg == "--help" || currentArg == "-h")
+                    {
+                        printUsage();
+                        Environment.Exit(0);
+                    }
+
+                    if (currentArg == "--version" || currentArg == "-v")
+                        Program.mcVersion = arguments[i + 1];
+
+                    if (currentArg == "--loader" || currentArg == "-l")
+                        Program.modLoader = arguments[i + 1];
+
+
+                }
+            }
+            catch(Exception e){ printUsage(); }
+        }
+
+        public void printUsage()
+        {
+            Console.WriteLine("Usage: mcmd path/to/mods/directory [options]\n");
+            Console.WriteLine("Note that path MUST be first argument.\nOptions:");
+            Console.WriteLine("--version MinecraftVersion, -v MinecraftVersion - Set minecraft version that you want to use. (1.16.5 by default)");
+            Console.WriteLine("--loader ModLoader, -l ModLoader - Set a mod loader that you want to use. (Forge by default)");
+            Console.WriteLine("--help, -h - Print this message.");
+        }
     }
 }
